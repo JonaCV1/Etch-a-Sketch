ac47ce641736fac3b7aa4ee6b1ca8907ed753825
Jona <jonathan.cortes.v@gmail.com>
2025-03-31 17:51:06 -0300

    Se modifican archivos index.html y script.js
    Se añade la posibilidad de usar colores al azar y escala de grises.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let defaultNumber = 16;
 let rowNumber = defaultNumber;
 let colNumber = rowNumber;
 let maxGridDimension = 800;
+let typePaint = "";
 const gridContainer = document.querySelector("div.grid-container");
 
 function drawGrid(rowNumber, colNumber) {
@@ -18,24 +19,68 @@ function drawGrid(rowNumber, colNumber) {
       rowContainer.appendChild(divGrid);
     }
   }
-
-  grids = document.querySelectorAll("div.div-grid");
-  grids.forEach((grid) => {
-    grid.addEventListener("mouseover", (event) => {
-      event.target.style.background = "black";
-    });
-  });
 }
 
 function deleteGrid() {
   gridContainer.innerHTML = "";
 }
 
-drawGrid(rowNumber, colNumber);
+function setSolidColor() {
+  grids = document.querySelectorAll("div.div-grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", (event) => {
+      event.target.style.background = "black";
+    });
+  });
+  typePaint = "solidColor";
+}
+
+function getRandomColorHex() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor.padStart(6, "0")}`;
+}
+
+function setRandomColor() {
+  grids = document.querySelectorAll("div.div-grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", (event) => {
+      event.target.style.background = getRandomColorHex();
+    });
+  });
+  typePaint = "randomColor";
+}
 
 const btnGridDimension = document.querySelector("button#grid-dimension");
-const btnResetDraw = document.querySelector("button#reset-draw");
+const btnSolidColor = document.querySelector("button#solid-color");
+const btnRandomColor = document.querySelector("button#random-color");
+const btnGrayScale = document.querySelector("button#gray-scale");
 const btnDefaultGrid = document.querySelector("button#default-grid");
+
+function setGrayColor() {
+  grids = document.querySelectorAll("div.div-grid");
+  grids.forEach((grid) => {
+    grid.style.opacity = 0;
+    grid.style.background = "black";
+    grid.addEventListener("mouseover", (event) => {
+      event.target.style.opacity = Math.min(
+        parseFloat(window.getComputedStyle(event.target).opacity) + 0.1,
+        1
+      );
+      event.target.style.background = "black";
+    });
+  });
+  typePaint = "grayColor";
+}
+
+function setTypePaint(typePaint) {
+  if (typePaint == "solidColor") {
+    return setSolidColor();
+  } else if (typePaint == "grayColor") {
+    return setGrayColor();
+  } else {
+    return setRandomColor();
+  }
+}
 
 btnGridDimension.addEventListener("click", () => {
   rowNumber = prompt("Type the number of row in the grid (max 100): ");
@@ -47,14 +92,32 @@ btnGridDimension.addEventListener("click", () => {
   colNumber = rowNumber;
   deleteGrid();
   drawGrid(rowNumber, colNumber);
+  setTypePaint(typePaint);
 });
 
-btnResetDraw.addEventListener("click", () => {
+btnSolidColor.addEventListener("click", () => {
   deleteGrid();
   drawGrid(rowNumber, colNumber);
+  setSolidColor();
+});
+
+btnRandomColor.addEventListener("click", () => {
+  deleteGrid();
+  drawGrid(rowNumber, colNumber);
+  setRandomColor();
+});
+
+btnGrayScale.addEventListener("click", () => {
+  setGrayColor();
 });
 
 btnDefaultGrid.addEventListener("click", () => {
   deleteGrid();
   drawGrid(defaultNumber, defaultNumber);
+  setTypePaint(typePaint);
+  rowNumber = defaultNumber;
+  colNumber = rowNumber;
 });
+
+drawGrid(rowNumber, colNumber);
+setSolidColor();
