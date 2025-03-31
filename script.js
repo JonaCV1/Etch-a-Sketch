b2c146658e7c55f8cc917f07c2ae61a9b3738c53
Jona <jonathan.cortes.v@gmail.com>
2025-03-31 13:16:27 -0300

    Se modifican los archivos index.html, script.js y style.css
    Se añaden los botones "Reset draw" y "Default grid" para borrar el dibujo y mantener la grilla y para volver a la grilla por defecto, además se añade la función de modificar la cantidad de celdas de la grilla considerando como míninmo una celda y como máximo 100, esto manteniendo el tamaño de la grilla.

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-let rowNumber = 16;
-let colNumber = 16;
+let defaultNumber = 16;
+let rowNumber = defaultNumber;
+let colNumber = rowNumber;
+let maxGridDimension = 800;
 const gridContainer = document.querySelector("div.grid-container");
 
-for (let i = 0; i < rowNumber; i++) {
-  let rowContainer = document.createElement("div");
-  rowContainer.setAttribute("class", "div-row");
-  gridContainer.appendChild(rowContainer);
-  for (let j = 0; j < colNumber; j++) {
-    let squareSize = 50;
-    let divGrid = document.createElement("div");
-    divGrid.style.width = `${squareSize}px`;
-    divGrid.style.height = `${squareSize}px`;
-    divGrid.setAttribute("class", "div-grid");
-    rowContainer.appendChild(divGrid);
+function drawGrid(rowNumber, colNumber) {
+  for (let i = 0; i < rowNumber; i++) {
+    let rowContainer = document.createElement("div");
+    rowContainer.setAttribute("class", "div-row");
+    gridContainer.appendChild(rowContainer);
+    for (let j = 0; j < colNumber; j++) {
+      let squareSize = maxGridDimension / rowNumber;
+      let divGrid = document.createElement("div");
+      divGrid.style.width = `${squareSize}px`;
+      divGrid.style.height = `${squareSize}px`;
+      divGrid.setAttribute("class", "div-grid");
+      rowContainer.appendChild(divGrid);
+    }
   }
+
+  grids = document.querySelectorAll("div.div-grid");
+  grids.forEach((grid) => {
+    grid.addEventListener("mouseover", (event) => {
+      event.target.style.background = "black";
+    });
+  });
 }
 
-grids = document.querySelectorAll("div.div-grid");
-grids.forEach((grid) => {
-  grid.addEventListener("mouseover", (event) => {
-    event.target.style.background = "blue";
-  });
-});
+function deleteGrid() {
+  gridContainer.innerHTML = "";
+}
 
-btnGridDimension = document.querySelector("button#grid-dimension");
+drawGrid(rowNumber, colNumber);
+
+const btnGridDimension = document.querySelector("button#grid-dimension");
+const btnResetDraw = document.querySelector("button#reset-draw");
+const btnDefaultGrid = document.querySelector("button#default-grid");
+
 btnGridDimension.addEventListener("click", () => {
   rowNumber = prompt("Type the number of row in the grid (max 100): ");
+  if (rowNumber <= 0) {
+    rowNumber = 1;
+  } else if (rowNumber >= 100) {
+    rowNumber = 100;
+  }
+  colNumber = rowNumber;
+  deleteGrid();
+  drawGrid(rowNumber, colNumber);
+});
+
+btnResetDraw.addEventListener("click", () => {
+  deleteGrid();
+  drawGrid(rowNumber, colNumber);
+});
+
+btnDefaultGrid.addEventListener("click", () => {
+  deleteGrid();
+  drawGrid(defaultNumber, defaultNumber);
 });
